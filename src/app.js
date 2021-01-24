79e5bad866536675e27208b9d7f244891c014c36
Matt Banak <banak.matt@gmail.com>
2021-01-23 16:23:47 -0800

    Added dotenv and environemnt variable for local media path

--- src/app.js
+++ src/app.js
@@ -1,17 +1,28 @@
+require('dotenv').config();
+
 var ae = require("after-effects");
 
 console.log("running...");
 
 const images = [
-    'C:/Users/banak/Pictures/tavern/image 1.jpg',
-    'C:/Users/banak/Pictures/tavern/image 2.jpg',
-    'C:/Users/banak/Pictures/tavern/tavern.jpg',
-    'C:/Users/banak/Pictures/tavern/download.jpg',
-    'C:/Users/banak/Pictures/tavern/The_Tavern.jpg'
+    'tavern/image 1.jpg',
+    'tavern/image 2.jpg',
+    'tavern/tavern.jpg',
+    'tavern/download.jpg',
+    'tavern/The_Tavern.jpg'
     ],
+    // Once music is included maybe set seconds per image and total time based on audio length?
+    // ie. audioLength / images.length (then pop images if necessary until seconds per image would be at least 4s)
     secondsPerImage = 5,
     totalTime = images.length * secondsPerImage,
-    compositionName = "Composition 1";
+    compositionName = "Composition 1",
+    mediaRoot = process.env.MEDIA_ROOT || '../media/';
+
+// Make image paths absolute
+for (i = 0; i < images.length; i++)
+{
+    images[i] = mediaRoot + images[i];
+}
 
 ae.execute((secondsPerImage, totalTime, compositionName, images) => 
 {
@@ -83,5 +94,5 @@ ae.execute((secondsPerImage, totalTime, compositionName, images) =>
     .then((returnedValue) => {console.log(returnedValue);})
     .catch((error) => {console.log(error)});
 
-    // Followup idea, use the Audition scripting to also generate a perfectly timed song
+    // Followup idea, use Audition scripting to also generate a perfectly timed song
     // there's not an npm package for that though, maybe could reuse this one
